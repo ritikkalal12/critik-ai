a3f18a2e5c71ab0e14b2fee9becb7b4b3322df97
Ritik Kalal <ritikkalal1234@gmail.com>
2026-07-13 20:03:34 +0530

    Connect Gateway with Auth Service

--- backend/gateway/index.js
+++ backend/gateway/index.js
@@ -1,10 +1,13 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import proxy from 'express-http-proxy';
 dotenv.config();
 
 const port = process.env.PORT;
 
 const app = express();
+
+app.use('/auth', proxy(process.env.AUTH_SERVICE_URL));
 
 app.get('/', (req, res) => {
   res.json({ message: 'Gateway server is running' });
