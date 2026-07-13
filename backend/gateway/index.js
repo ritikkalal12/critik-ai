50fc3f5329f6877d242258b7c4693291277ef76c
Ritik Kalal <ritikkalal1234@gmail.com>
2026-07-13 19:25:23 +0530

    Creating Gateway Server

--- backend/gateway/index.js
+++ backend/gateway/index.js
@@ -1,2 +1,15 @@
 import express from 'express';
 import dotenv from 'dotenv';
+dotenv.config();
+
+const port = process.env.PORT;
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.json({ message: 'Gateway server is running' });
+});
+
+app.listen(port, () => {
+  console.log(`Gateway server is running on port ${port}`);
+});
